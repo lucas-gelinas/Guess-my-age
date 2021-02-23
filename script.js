037e4efb330d9d0d6265fc13ac7d44f33b2e42d8
Lucas Gelinas <lucas.gelinas@ocsbstudent.ca>
2021-02-23 18:53:53 +0000

    For sure final fix

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let age = 0
 const person = prompt('Please enter your name', '[NAME]')
 
 if (person != null) {
-  document.getElementById("demo").innerHTML =
+  document.getElementById('demo').innerHTML =
   'Hello, ' + person + '! Welcome to this mini guessing game! If you can guess the age of my creator, you win... um... something! Yeah! Your hints are...'
 
   document.getElementById('demo2').innerHTML = 'This creator is 5 feet, 9 inches tall. He is also still in school, the tenth grade.'
@@ -14,13 +14,13 @@ function check () {
   // Taking user input to display appropriate message
 
   while (age !== 15) {
-    age = prompt ('Take a guess!')
+    age = prompt('Take a guess!')
     age = parseInt(age)
   } if (age > 15) {
-  alert ('Nope, too high! You gotta try again.')
+  alert('Nope, too high! You gotta try again.')
   } else if (age < 15) {
-  alert ('Nope, too low! You gotta try again.')
+  alert('Nope, too low! You gotta try again.')
   } else if (age === 15) {
-  alert ('You got it right! Congratulations, you win... um... bragging rights! Yeah, that there is your prize!')
+  alert('You got it right! Congratulations, you win... um... bragging rights! Yeah, that there is your prize!')
   }
 }
